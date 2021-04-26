8785fd4fe220c27ecdc48d3628d562844b89d64c
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-26 09:41:59 -0400

    add dependencies and configure eslint

--- api/server.js
+++ api/server.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
-const server = express()
-server.use(express.json())
-server.use(helmet())
-server.use(cors())
+const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
-    message: err.message
+    message: err.message,
   });
 });
 
-module.exports = server
+module.exports = server;
