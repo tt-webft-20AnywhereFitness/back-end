1b6a7ad37284d91e33552e1bb56832c4f3389738
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-26 10:53:48 -0400

    migration notes

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,3 +1,11 @@
+// We will have four tables
+// USERS TABLE -- user_id, username, password, role_id, remaining_classes
+// CLASSES TABLE -- class_id, class_name, type, startTime (possible integer?)
+// duration (integer), intensity, location, registered_clients, size
+// ROLES TABLE - role_id, role_name
+// REGISTRATIONS TABLE - registration_id, class_id,
+// user_id(can be named client_id but it will be a foreign key referencing)
+
 exports.up = async (knex) => {
   await knex.schema
     .createTable('users', (users) => {
