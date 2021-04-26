a53585c24e2d4af0cf5be8a7b3d92d58635b47c3
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-26 09:39:09 -0400

    create files

--- api/server.js
+++ api/server.js
@@ -7,4 +7,10 @@ server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message
+  });
+});
+
 module.exports = server
