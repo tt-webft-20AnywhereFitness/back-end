99b0352870a65bfaf5d570c4fef5e61b27dea194
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-26 10:00:20 -0400

    Routers connected

---
+++ api/users/users-router.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+router.post('/register', (req, res, next) => {
+  console.log('User created...');
+  console.log(`Payload is: ${req.body}`);
+});
+
+router.post('/login', (req, res, next) => {
+  console.log('User logged in...');
+  console.log(`Payload is: ${req.body}`);
+});
