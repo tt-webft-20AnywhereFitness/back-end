f14a42f9e2e41a6551818446bfc79f58b0c073e8
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-27 10:20:25 -0400

    validateCredentials middleware

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,13 +1,33 @@
 const router = require('express').Router();
-const { checkUsernameExists, checkEmailExists, validateRoleName } = require('./auth-middleware');
+const bcrypt = require('bcryptjs');
+const {
+  checkUsernameExists,
+  checkEmailExists,
+  validateRoleName,
+  validateCredentials
+} = require('./auth-middleware');
 const { JWT_SECRET } = require('../secrets/secrets');
+const Auth = require('./auth-model');
 
-router.post('/register', (req, res, next) => {
+router.post(
+  '/register',
+  checkEmailExists,
+  checkUsernameExists,
+  async (req, res, next) => {
+    const userInfo = req.body;
+    const hash = bcrypt.hashSync(userInfo.password, 8);
+    userInfo.password = hash;
 
-});
+    Auth.addUser(userInfo)
+      .then((user) => {
+        res.status(201).json(user);
+      })
+      .catch(next);
+  },
+);
 
-router.post('/login', (req, res, next) => {
-
+router.post('/login', validateCredentials, (req, res, next) => {
+  if (bcrypt.compareSync(req.body.password, req.user.password))
 });
 
 router.post('logout', (req, res, next) => {
