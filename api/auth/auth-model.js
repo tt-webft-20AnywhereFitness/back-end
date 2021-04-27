1e39edf141bc9e5f73c726057cc41e90319abb9f
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-27 10:02:33 -0400

    addUser model function

--- api/auth/auth-model.js
+++ api/auth/auth-model.js
@@ -1,9 +1,16 @@
 const db = require('../data/db-config');
 
 function getBy(filter) {
-    return db('users').where(filter);
+  return db('users').where(filter).first();
+}
+
+async function addUser(userInfo) {
+  const newUser = await db('users').insert(userInfo)
+    .then(([id]) => db('users').where({ id }).first());
+  return newUser;
 }
 
 module.exports = {
-    getBy,
+  getBy,
+  addUser,
 };
