2b2b1089a4efb112ce6316be31cc05a829832156
Domenic Scarcella <ls41513@yahoo.com>
2021-04-29 16:58:43 -0400

    Touch-ups and comments added to auth-middleware.js, auth-models.js, auth-router.js

--- api/auth/auth-model.js
+++ api/auth/auth-model.js
@@ -1,9 +1,12 @@
 const db = require("../data/db-config");
 
+
+// GENERAL SEARCH
 function getBy(filter) {
   return db("users").where(filter);
 }
 
+// ID-SPECIFIC SEARCH
 const getById = (user_id) => {
   if (typeof user_id !== "number") {
     user_id = parseInt(user_id);
@@ -11,6 +14,7 @@ const getById = (user_id) => {
   return db("users").where({ user_id }).first();
 };
 
+// NEW USER
 function addUser(userInfo) {
   return db("users")
     .insert(userInfo)
